5f4098467522ab75a44d3fe2c2a68da2ded6c738
Deltapg001 <lassipiloop@proton.me>
2025-03-06 16:05:25 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @Rishi747
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8057194164:AAGyQ0Bxhvalo5bw9NOO-FFXcSrXsBuTj_w")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@JackHubxOwner")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7757502461")
+
+
+
+
+
+
+
+  
